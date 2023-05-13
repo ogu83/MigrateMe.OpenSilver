e43e852bc4d22a8a15ecd7c62cf2f6e89363043b
oguzkoroglu <oguzkoroglu@gmail.com>
2023-05-13 03:59:51 +0300

    Add project files.

---
+++ MigrateMe.Controls/SpecialButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MigrateMe.Controls
+{
+    public class SpecialButton : Button
+    {
+        public SpecialButton()
+        {
+            this.DefaultStyleKey = typeof(SpecialButton);
+        }
+    }
+}
---
+++ MigrateMe.Controls/SpecialClock.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MigrateMe.Controls
+{
+    public partial class SpecialClock : UserControl
+    {
+        public SpecialClock()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            txtClock.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
